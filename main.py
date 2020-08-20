b7ffb56ae0f0232879061a9aec1ab7f4e9301eac
Nikhil Yadav <nikhil27rock@gmail.com>
2020-08-20 21:27:15 +0530

    last commit

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ soup = BeautifulSoup(htmlContent, 'html.parser')
 # print(type(title)) # 1. Tag
 # print(type(title.string)) # 2. NavigableString
 # print(type(soup)) # 3. BeautifulSoup
+
 # 4. Comment
 # markup = "<p><!-- this is a comment --></p>"
 # soup2 = BeautifulSoup(markup)
@@ -61,3 +62,28 @@ for link in anchors:
         linkText = "https://codewithharry.com" +link.get('href')
         all_links.add(link)
         # print(linkText)
+
+navbarSupportedContent = soup.find(id='navbarSupportedContent')
+
+# .contents - A tag's children are available as a list
+# .children - A tag's children are available as a generator
+# for elem in navbarSupportedContent.contents:
+#     print(elem)
+
+# for item in navbarSupportedContent.strings:
+#     print(item)
+
+# for item in navbarSupportedContent.stripped_strings:
+#     print(item)
+
+# print(navbarSupportedContent.parent)
+# for item in navbarSupportedContent.parents: 
+#     print(item.name)
+
+# print(navbarSupportedContent.next_sibling.next_sibling)
+# print(navbarSupportedContent.previous_sibling.previous_sibling)
+
+# elem = soup.select('.modal-footer')
+# print(elem)
+elem = soup.select('#loginModal')[0] 
+print(elem)
